c4a24a6426ee1f4049ae72842fba7a8ef534abc6
esthi <hoeuyu@student.ethz.ch>
2022-04-24 21:58:32 +0200

    Update AddPersonForm.js

--- src/components/AddPersonForm.js
+++ src/components/AddPersonForm.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addPerson } from '../store/people/actions'
+import { addMembers } from '../store/people/actions'
 
 function AddPersonForm(props) {
     const [ person, setPerson ] = React.useState({})
     
     function handleChange(event) {
-        let newPerson = { 
+        let newMember= { 
             name: event.target.value 
         }
         setPerson(newPerson)
@@ -32,3 +32,74 @@ const mapDipatchToProps = {
 }
 
 export default connect(null, mapDipatchToProps)(AddPersonForm)
+
+Reagieren von 'reagieren' importieren
+importiere {connect} aus 'react-redux'
+importiere { addMembers } aus '../store/people/actions'
+
+Funktion AddPersonForm(Requisiten) {
+     const [ person, setPerson ] = React.useState({})
+    
+     Funktion handleChange (Ereignis) {
+         let newMember= {
+             Name: Ereignis.Zielwert.Wert
+         }
+         setPerson(neuePerson)
+     }
+    
+     Funktion HandleSubmit(Ereignis) {
+         // Verhindert das Standardverhalten des Submit-Ereignisses (aktualisiert die Seite)
+         event.preventDefault()
+        
+         props.boundAddPerson(Person)
+         setPerson({ name: "" })
+     }
+     Rückkehr (
+         <form onSubmit={handleSubmit}>
+             <input type="text" onChange={handleChange} value={person.name} />
+             <button type="submit">Senden</button>
+         </form>
+     )
+}
+
+const mapDipatchToProps = {
+   gebundenAddPerson: addPerson
+}
+
+export default connect(null, mapDipatchToProps)(AddPersonForm)
+
+/*
+Reagieren von 'reagieren' importieren
+importiere {connect} aus 'react-redux'
+importiere { addMembers } aus '../store/people/actions'
+
+Funktion AddPersonForm(Requisiten) {
+     const [ person, setPerson ] = React.useState({})
+    
+     Funktion handleChange (Ereignis) {
+         let newMember= {
+             Name: Ereignis.Zielwert.Wert
+         }
+         setPerson(neuePerson)
+     }
+    
+     Funktion HandleSubmit(Ereignis) {
+         // Verhindert das Standardverhalten des Submit-Ereignisses (aktualisiert die Seite)
+         event.preventDefault()
+        
+         props.boundAddPerson(Person)
+         setPerson({ name: "" })
+     }
+     Rückkehr (
+         <form onSubmit={handleSubmit}>
+             <input type="text" onChange={handleChange} value={person.name} />
+             <button type="submit">Senden</button>
+         </form>
+     )
+}
+
+const mapDipatchToProps = {
+   gebundenAddPerson: addPerson
+}
+
+export default connect(null, mapDipatchToProps)(AddPersonForm) */
